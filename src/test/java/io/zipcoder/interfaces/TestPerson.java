b42a63babffe366a9dc507fd9f611f2db9dc1980
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-11-21 17:19:51 -0500

    part 1

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,24 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void testConstructor() {
+        long eid = 123;
+        String en = "";
+        Person person = new Person(eid, en);
+        Assert.assertEquals(eid,person.getId());
+        Assert.assertEquals(en, person.getName());
+    }
+    @Test
+    public void testSetName(){
+        String en = "";
+        Person p = new Person(123, "asdf");
+        p.setName(en);
+        Assert.assertEquals(en,p.getName());
+    }
 }
